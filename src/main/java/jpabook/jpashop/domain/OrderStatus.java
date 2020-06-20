3ac94efac11d6971969599cfc7f5e6123190ae73
YoungdonJung <yeavov@gmail.com>
2020-06-20 16:18:34 +0900

    Order functions develope finish

--- src/main/java/jpabook/jpashop/domain/OrderStatus.java
+++ src/main/java/jpabook/jpashop/domain/OrderStatus.java
@@ -1,5 +1,5 @@
 package jpabook.jpashop.domain;
 
-public enum OerderStatus {
+public enum OrderStatus {
     ORDER, CANCEL
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 
+
 import jpabook.jpashop.domain.Category;
 import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
@@ -13,6 +14,7 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
+@Setter
 public abstract class Item {
 
     @Id
