71e99b442b26401592956fcf8756fff078cafc56
youngdon <youngdon.jung@lotte.net>
2024-02-02 11:11:51 +0900

    New Log Trace

---
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -0,0 +1,15 @@
+package hello.advanced;
+
+import hello.advanced.app.trace.logtrace.FieldLogTrace;
+import hello.advanced.app.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace(){
+        return new FieldLogTrace();
+    }
+}
