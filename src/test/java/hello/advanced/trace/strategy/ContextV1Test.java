7cf07661bc1a96a47033bf3546b39eb0778579bf
youngdon <youngdon.jung@lotte.net>
2024-02-06 11:00:51 +0900

    Strategy pattern 2

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -44,5 +45,65 @@ public class ContextV1Test {
         ContextV1 contextV2 = new ContextV1(strategyLogic2);
         contextV2.execute();
 
+    }
+
+    @Test
+    void strategyV2(){
+        Strategy strategyLogic1 = new Strategy(){
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        };
+
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+
+        Strategy strategyLogic2 = new Strategy(){
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        };
+
+        ContextV1 contextV2 = new ContextV1(strategyLogic1);
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV3(){
+
+        ContextV1 contextV1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        });
+        contextV1.execute();
+
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        });
+
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV4(){
+
+        ContextV1 contextV1 = new ContextV1(() -> log.info("비즈니스 로직 1 실행"));
+        contextV1.execute();
+
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비즈니스 로직 2 실행"));
+
+        contextV2.execute();
     }
 }
